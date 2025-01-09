5f49e285032b5a202c4649bd9e3b0053afd72b3f
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2025-01-09 14:46:50 +0100

    feat: populated hard coded home data

--- api/src/v1/home/services/home_service.py
+++ api/src/v1/home/services/home_service.py
@@ -10,12 +10,16 @@ class HomeService:
     def get_home_data(self):
         # Hardcoded data for now
         return {
-            "submissionFee": "10€",
-            "lectureFreeTime": "12.10 - 12.02",
-            "lectureTime": "12.02 - 13.03",
+            "submissionFee": "8.02.25: 85,00€",
+            "lectureFreeTime": "14.10.24 - 7.02.25",
+            "lectureTime": "7.02.25 - 23.04.25",
             "links": [
-                {"title": "LMU", "url": "https://www.lmu.de"},
-                {"title": "LMU", "url": "https://www.lmu.de"},
-                {"title": "LMU", "url": "https://www.lmu.de"},
+                {"title": "LMU-Portal", "url": "https://www.lmu.de/lmu-intern/"},
+                {"title": "Moodle", "url": "https://www.lmu.de/raumfinder/#/"},
+                {"title": "Raumfinder", "url": "https://www.lmu.de"},
+                {"title": "Immatrikulation", "url": "https://qissos.verwaltung.uni-muenchen.de/qisserversos/rds?state=change&type=1&moduleParameter=studentReportsMenu&nextdir=change&next=menu.vm&xml=menu&purge=y&subdir=qissos/reports&menuid=qissosreportsPublish&breadcrumb=qissosreports&breadCrumbSource=menu"},
+                {"title": "LMU-Mail", "url": "https://mailbox.portal.uni-muenchen.de/webmail/webmail/ui/MainPage.html"},
+                {"title": "Beitragskonto", "url": "https://qissos.verwaltung.uni-muenchen.de/qisserversos/rds?state=gebkonto&asi=a$Lx1jxFA$kMP5tPiuVP"},
+                {"title": "Benutzerkonto", "url": "https://www.portal.uni-muenchen.de/benutzerkonto/#!/"},
             ]
         }
