e397dfdc79a6289c4c3fe50c42964e44ca5cd2f7
Tim <62808178+timothymaxwellsummers@users.noreply.github.com>
2025-01-09 16:43:16 +0100

    fix: links in home endpoint

--- api/src/v1/home/services/home_service.py
+++ api/src/v1/home/services/home_service.py
@@ -15,8 +15,8 @@ class HomeService:
             "lectureTime": "7.02.25 - 23.04.25",
             "links": [
                 {"title": "LMU-Portal", "url": "https://www.lmu.de/lmu-intern/"},
-                {"title": "Moodle", "url": "https://www.lmu.de/raumfinder/#/"},
-                {"title": "Raumfinder", "url": "https://www.lmu.de"},
+                {"title": "Moodle", "url": "https://moodle.lmu.de/"},
+                {"title": "Raumfinder", "url": "https://www.lmu.de/raumfinder/#/"},
                 {"title": "Immatrikulation", "url": "https://qissos.verwaltung.uni-muenchen.de/qisserversos/rds?state=change&type=1&moduleParameter=studentReportsMenu&nextdir=change&next=menu.vm&xml=menu&purge=y&subdir=qissos/reports&menuid=qissosreportsPublish&breadcrumb=qissosreports&breadCrumbSource=menu"},
                 {"title": "LMU-Mail", "url": "https://mailbox.portal.uni-muenchen.de/webmail/webmail/ui/MainPage.html"},
                 {"title": "Beitragskonto", "url": "https://qissos.verwaltung.uni-muenchen.de/qisserversos/rds?state=gebkonto&asi=a$Lx1jxFA$kMP5tPiuVP"},
