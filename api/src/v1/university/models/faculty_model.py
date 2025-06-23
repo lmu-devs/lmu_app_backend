c85409547983fe67350427c1394b2f29af7620fd
Lyrexes <steamkonto60@gmail.com>
2025-06-23 21:28:58 +0200

    Added endpoint for universites and faculties

--- api/src/v1/university/models/faculty_model.py
+++ api/src/v1/university/models/faculty_model.py
@@ -1,23 +1,10 @@
 from typing import List
-
 from pydantic import BaseModel, RootModel
-
-from shared.src.enums import FacultyEnum
-from shared.src.tables import FacultyTable, FacultyTranslationTable
 
 
 class Faculty(BaseModel):
-    id: FacultyEnum
+    id: int
     title: str
-
-    @classmethod
-    def from_table(cls, faculty: FacultyTable) -> "Faculty":
-        translation: FacultyTranslationTable = faculty.translations[0] if faculty.translations else "Not translated"
-
-        return Faculty(
-            id=FacultyEnum(faculty.id),
-            title=translation.title,
-        )
 
 
 class Faculties(RootModel):
--- api/src/v1/university/models/university_model.py
+++ api/src/v1/university/models/university_model.py
@@ -1,23 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, RootModel
+from typing import List
 
 from shared.src.enums import UniversityEnum
 from shared.src.tables import UniversityTable, UniversityTranslationTable
-
-from .faculty_model import Faculties
 
 
 class University(BaseModel):
     id: UniversityEnum
     title: str
-    faculties: Faculties | None = None
 
-    @classmethod
-    def from_table(cls, university: UniversityTable) -> "University":
-        translation: UniversityTranslationTable = (
-            university.translations[0] if university.translations else "Not translated"
-        )
 
-        return University(
-            id=UniversityEnum(university.id),
-            title=translation.title,
-        )
+class Universities(RootModel):
+    root: List[University] | list = []
