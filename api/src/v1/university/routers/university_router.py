e891449500d22d44d256f0dc51c98dd0938d0307
Lyrexes <steamkonto60@gmail.com>
2025-07-09 18:49:12 +0200

    fix: Added language default value and handling en or de in header properly

--- api/src/v1/university/routers/university_router.py
+++ api/src/v1/university/routers/university_router.py
@@ -12,9 +12,10 @@ router = APIRouter()
     response_model=Faculties,
     description="Get all faculty titles and names",
 )
-async def get_faculties(accept_language: str = Header())-> Faculties:
+async def get_faculties(accept_language: str = Header("de-DE"))-> Faculties:
     """Fetches all faculties with their titles and ids."""
-    university_service = UniversityService(accept_language)
+    language = map_language(accept_language)
+    university_service = UniversityService(language)
     faculties = await university_service.get_faculties()
     return faculties
 
@@ -24,7 +25,16 @@ async def get_faculties(accept_language: str = Header())-> Faculties:
     response_model=Universities,
     description="Get all universities",
 )
-async def get_universities(accept_language: str = Header()) -> Universities:
-    university_service = UniversityService(accept_language)
+async def get_universities(accept_language: str = Header("de-DE")) -> Universities:
+    language = map_language(accept_language)
+    university_service = UniversityService(language)
     universities = await university_service.get_universities()
     return universities
+
+def map_language(language: str) -> str:
+    if language == "de":
+        return "de-DE"
+    elif language == "en":
+        return "en-US"
+    else:
+        return language
