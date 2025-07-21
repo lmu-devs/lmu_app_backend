2b127c6494411c6e6b2f6d3a573d610d7360815a
SkyHorizon3 <neuner.r@gmx.de>
2025-07-21 18:13:17 +0200

    fix: cleanup test user stuff

--- api/src/v1/calendar/routers/calendar_router.py
+++ api/src/v1/calendar/routers/calendar_router.py
@@ -18,14 +18,12 @@ from shared.src.tables import UserTable
 
 router = APIRouter()
 
-TESTID = uuid.UUID("0cc1152e-c8dc-4ffe-995f-e6638b72704f")
-
 @router.post("/calendar-create-user", response_model=CalendarEntries, description="Create a calendar event for an user.")
 async def create_event_user(
     calendar_data: CalendarCreate,
-    #user: UserTable = Depends(APIKey.verify_user_api_key)
+    user: UserTable = Depends(APIKey.verify_user_api_key)
 ):
-    entries = CalendarService().create_event(TESTID, calendar_data)
+    entries = CalendarService().create_event(user.id, calendar_data)
     return CalendarEntries.from_list(entries)
 
 @router.post("/calendar-create-public", response_model=CalendarEntries, description="Create a calendar event for all users.")
@@ -41,9 +39,9 @@ async def update_event(
     event_id: uuid.UUID,
     recurrence_id: Optional[int] = None,
     update_type: UpdateType = UpdateType.THIS,
-    #user: UserTable = Depends(APIKey.verify_user_api_key)
+    user: UserTable = Depends(APIKey.verify_user_api_key)
 ):
-    entries = CalendarService().update_event(TESTID, event_id, recurrence_id, calendar_data, update_type)
+    entries = CalendarService().update_event(user.id, event_id, recurrence_id, calendar_data, update_type)
     return CalendarEntries.from_list(entries)
 
 @router.put("/calendar-update-public", response_model=CalendarEntries, description="Update a calendar event available for all users.")
@@ -67,8 +65,8 @@ async def get_events(
     event_type: Optional[EventType] = None,
     frequency: Optional[str] = None,
     all_day: Optional[bool] = None,
-    access_scope: AccessScope = AccessScope.USER
-    #user: UserTable = Depends(APIKey.verify_user_api_key)
+    access_scope: AccessScope = AccessScope.USER,
+    user: UserTable = Depends(APIKey.verify_user_api_key)
 ):
-    events = CalendarService().get_all(TESTID, access_scope, event_type, frequency, all_day)
+    events = CalendarService().get_all(user.id, access_scope, event_type, frequency, all_day)
     return CalendarEntries.from_list(events)
