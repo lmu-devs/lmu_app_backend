89be729a3a8e2160c5761c76513c767375c501f1
rubenmaria <steamkonto60@gmail.com>
2025-08-01 21:28:53 +0200

    fix: collector now works as expected with lsf update

--- api/src/v1/classes/routers/lecture_router.py
+++ api/src/v1/classes/routers/lecture_router.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter, Depends
 
 from ..services.lecture_service import LectureService
@@ -17,25 +19,16 @@ router = APIRouter()
 )
 async def get_courses_from_faculty(
     faculty_id: int = 1,
-    db: AsyncSession = Depends(get_async_db),
+    term_id: int = 1,
+    year: int = datetime.datetime.now().year,
+    session: AsyncSession = Depends(get_async_db),
 ) -> LecturesBasic:
     """Endpoint to get course from a specific faculty."""
     lecture_service = LectureService()
-    result = await lecture_service.get_lectures_from_faculty_db(faculty_id, db)
-    print(f"amount of lectures: {len(result.root)}")
-    return result
-
-
-@router.get(
-    "/course-by-faculty-old",
-    response_model=LecturesBasic,
-    description="Get all courses from a specified faculty",
-)
-async def get_courses_from_faculty_old(
-    faculty_id: int = 1,
-) -> LecturesBasic:
-    """Endpoint to get course from a specific faculty."""
-    lecture_service = LectureService()
-    result = await lecture_service.get_lectures_from_faculty(faculty_id)
-    print(f"amount of lectures: {len(result.root)}")
+    result = await lecture_service.get_lectures_from_faculty_db(
+        session,
+        faculty_id,
+        year,
+        term_id
+    )
     return result
