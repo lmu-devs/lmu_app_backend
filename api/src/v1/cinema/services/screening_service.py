ede2acadf1feee00165f32e614ce2e961f520690
raffi.git <51893700+rpgraffi@users.noreply.github.com>
2025-08-03 21:25:03 +0200

    chore: remove university-related tables and services from the data fetcher

--- api/src/v1/cinema/services/screening_service.py
+++ api/src/v1/cinema/services/screening_service.py
@@ -14,8 +14,6 @@ from shared.src.tables import (
     MovieTrailerTable,
     MovieTrailerTranslationTable,
     MovieTranslationTable,
-    UniversityTable,
-    UniversityTranslationTable,
 )
 from shared.src.tables.cinema.screening_table import ScreeningLikeTable
 
@@ -62,10 +60,6 @@ class ScreeningService:
                 .contains_eager(MovieTable.trailers)
                 .contains_eager(MovieTrailerTable.translations),
             )
-            # University and its relationships
-            .join(MovieScreeningTable.university)
-            .outerjoin(UniversityTable.translations)
-            .options(contains_eager(MovieScreeningTable.university).contains_eager(UniversityTable.translations))
             # Cinema and its relationships
             .join(MovieScreeningTable.cinema)
             .outerjoin(CinemaTable.translations)
@@ -85,7 +79,6 @@ class ScreeningService:
         return query.order_by(
             MovieScreeningTable.date,
             create_translation_order_case(MovieTranslationTable, language),
-            create_translation_order_case(UniversityTranslationTable, language),
             create_translation_order_case(MovieTrailerTranslationTable, language),
             create_translation_order_case(CinemaTranslationTable, language),
         )
